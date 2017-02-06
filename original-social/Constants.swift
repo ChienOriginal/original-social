4e046346f4faa99815870f5f7995b5d4f9ac9509
Chien Original <chienwang.original@gmail.com>
2017-02-05 21:18:14 -0800

    Sign In custom style

---
+++ original-social/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  original-social
+//
+//  Created by Chien Wang on 2/5/17.
+//  Copyright © 2017 Chien Wang. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
+let KEY_UID = "uid"
